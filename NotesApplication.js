0da20b84bf980f55ecefe07f50e00e55f7e05c3d
Morolake Orekoya <morekoya@gmail.com>
2016-05-17 16:55:44 +0100

    finish assignment

---
+++ NotesApplication.js
@@ -0,0 +1,52 @@
+function NotesApplication(author) {
+	this.author = author; // Take in a parameter author as the author of the note and saves this as an instance variable
+
+	this.notes = []; // Create a notes list to store all the notes as an instance property.
+	
+	// create(note_content) - This function takes the note content as the parameter and adds it to the notes list of the object.
+	this.create = function (note_content){
+		this.notes.push(note_content);
+	};
+	
+	// listNotes() - This function lists out each of the notes in the notes list in the following format. The note_id parameter below represents the respective index of each of the items in the list, the NOTE_CONTENT represent the note content and the author represents the note author.
+	this.listNotes  = function () {
+		for(var index = 0; index < this.notes.length; index++) {
+			console.log(
+				"Note ID: " + index + "\n" +
+				this.notes[index] + "\n" +
+				"By Author: " + this.author + "\n\n"
+			);
+			/*document.write(
+				"Note ID: " + index + "<br/>" +
+				this.notes[index] + "<br/>" +
+				"By Author: " + this.author + "<br/><br/>"
+			);*/
+		}
+	};
+	
+	// get(note_id) - This function takes a note_id which refers to the index of the note in the notes list and returns the content of that note as a string.
+	this.get = function (note_id) {
+		return this.notes[note_id];
+	};
+	
+	// search(search_text) - This function take a search string, search_text and returns all the notes with that text within it in the following format
+	this.search = function (search_text) {
+		result = [];
+		//var index;
+		for(index = 0; index < this.notes.length; index++) {
+			if (this.notes[index].includes(search_text)) {
+				result.push(this.notes[index]);
+			}
+		}
+		return result;
+	};
+}
+
+// Checking to make sure code works
+var oop = new NotesApplication("Morolake");
+oop.create("OOP stands for Object Oriented Programming. OOP is a way of programming that makes use of objects with attributes and behaviours(methods)");
+oop.create("The main features of OOP are: Data Encapsulation, Inheritance and Polymorphism");
+oop.create("Some OOP languages are JavaSript, Python, Java, Ruby, C/C++, etc");
+oop.listNotes();
+console.log("\nResult of running get: " + oop.get(0));
+console.log("\nResult of running search: " + oop.search("main"));	
